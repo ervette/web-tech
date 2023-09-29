41fcb1ef07f3b70de3206d7dc3169a1a28e9ceee
ervette <kiriltalalayko@gmail.com>
2023-09-29 13:38:15 +0100

    General set-up continuing

--- website/__init__.py
+++ website/__init__.py
@@ -3,6 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 import os
 from flask_login import LoginManager
+import openai
+
+
+
 
 
 db = SQLAlchemy()
@@ -14,11 +18,26 @@ def create_app():
     app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.getenv("DB_NAME")}'
     db.init_app(app)
+    openai.api_key= os.getenv("OPENAI_API_KEY")
+    openai.api_version
+
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo", 
+        messages=[{"role": "user", "content": "Write me a 50 words essay about ChatGPT 3.5 Turbo"}]
+        )
+
+    print(completion)
+    
     
     @app.route('/')
     def halo():
-        return "<h1>" + os.getenv("TEST") + "</h1>"
+        # return "<h1><a href=" + "/hello" + ">say hello</a></h1>"
+        return "<h1>" + completion + "</h1>"
         
+        
+    @app.route('/hello')
+    def hallowor():
+        return "<h1>Heloo world</h1>"
     
     return app
    
