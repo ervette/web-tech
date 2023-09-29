82d24b213878a497e2f12781f8b2060baeaadeea
ervette <kiriltalalayko@gmail.com>
2023-09-29 13:43:06 +0100

    dependencies update

--- website/__init__.py
+++ website/__init__.py
@@ -23,7 +23,7 @@ def create_app():
 
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo", 
-        messages=[{"role": "user", "content": "Write me a 50 words essay about ChatGPT 3.5 Turbo"}]
+        messages=[{"role": "user", "content": "Write me a 50 words essay about ChatGPT 3.5 Turbo greeting the customer"}]
         )
 
     print(completion)
