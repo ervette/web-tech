ac9a4d1449bab26031389e4b0aa1bbe7af5bcda0
ervette <kiriltalalayko@gmail.com>
2023-10-01 13:23:37 +0100

    1. Created Jinja HTML templates 2. Created Login, Sign-up, Home, Nav-bar documents 3. Initialised post request space 4. Created .js file for upcoming http requests 5. Added css bootstrap files

--- website/__init__.py
+++ website/__init__.py
@@ -5,44 +5,41 @@ import os
 from flask_login import LoginManager
 import openai
 
-db = SQLAlchemy() # Initializing database
+db = SQLAlchemy()  # Initializing database
 
 
 def create_app():
-    load_dotenv() #Loading .env variables
-    
-    #Flask settings
+    load_dotenv()  # Loading .env variables
+
+    # Flask settings
     app = Flask(__name__)
     app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
-    
-    #Database settings
+
+    # Database settings
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.getenv("DB_NAME")}'
     db.init_app(app)
-    
+
     # ---------------------------------------------------------------
     # OpenAI API settings + data gathering
-    
+
     # openai.api_key = os.getenv("OPENAI_API_KEY")
     # openai.api_version
 
     # completion = openai.ChatCompletion.create(
-    #     model="gpt-3.5-turbo", 
+    #     model="gpt-3.5-turbo",
     #     messages=[{"role": "user", "content": "Hello!"}]
     #     )
 
     # print(completion)
-    
-    
+
     # TO RETRIEVE DATA => str(completion.choices[0].message.content)
-    
+
     # ---------------------------------------------------------------
-    
+
     from .views import views
     from .auth import auth
-    
+
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
-    
-    return app
-   
 
+    return app
--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,40 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return "<p>Login</p>"
+    data = request.form
+    print(data)
+    return render_template("login.html", usr="ervette")
+
 
 @auth.route('/logout')
 def logout():
     return "<p>Logout</p>"
 
-@auth.route('sign-up')
+
+@auth.route('sign-up', methods=['GET', 'POST'])
 def sign_up():
-    return "<p>Sign Up</p>"
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        valid = False
+        if len(email) < 4:
+            pass
+        elif len(firstName) < 2:
+            pass
+        elif password1 != password2:
+            pass
+        elif len(password1) < 7:
+            pass
+        else:
+            # add to database
+            valid = True
+            pass
+
+    return render_template("sign_up.html", boolean = True)
