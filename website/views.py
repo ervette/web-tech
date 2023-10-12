abad517797f8d1037cfc353480bdc4a4365c149c
ervette <kiriltalalayko@gmail.com>
2023-10-12 12:05:30 +0100

    1. Added the .json dependency 2. Created Kanban board rendering logic 3. Used the new bootstrap version 4. Created sample backlog and used AI to generaate advanced kanban board

--- website/views.py
+++ website/views.py
@@ -1,7 +1,11 @@
 from flask import Blueprint, render_template
+import json
 
 views = Blueprint('views', __name__)
 
+
 @views.route('/')
 def home():
-    return render_template("home.html")
+    with open('website/kanban.json', 'r') as file:
+        data = json.load(file)
+    return render_template("home.html", data=data)
